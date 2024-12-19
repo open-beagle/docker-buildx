a1ef3dbe78aa1b370c9eda8bad390a0b625ee5e9
shucheng <shucheng@bd-apaas.com>
2024-12-19 12:28:12 +0800

    v0.19.2

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -12,13 +12,13 @@ export GOARCH=arm64
 bash hack/build
 mv bin/build/docker-buildx bin/build/docker-buildx-linux-$GOARCH
 
-export GOARCH=ppc64le
-bash hack/build
-mv bin/build/docker-buildx bin/build/docker-buildx-linux-$GOARCH
+# export GOARCH=ppc64le
+# bash hack/build
+# mv bin/build/docker-buildx bin/build/docker-buildx-linux-$GOARCH
 
-export GOARCH=mips64le
-bash hack/build
-mv bin/build/docker-buildx bin/build/docker-buildx-linux-$GOARCH
+# export GOARCH=mips64le
+# bash hack/build
+# mv bin/build/docker-buildx bin/build/docker-buildx-linux-$GOARCH
 
 export GOARCH=loong64
 bash hack/build
